7a9c31a0ef7fd3ec865559f1bb917ccf9b146880
Paulo Henrique Lerbach Rodrigues <paulohl@mirante.coop.br>
2016-12-15 18:11:01 -0200

    ADD: trying to predict unknown values

--- src/main/java/br/com/sitedoph/spark_examples/Person.java
+++ src/main/java/br/com/sitedoph/spark_examples/Person.java
@@ -1,16 +1,20 @@
 package br.com.sitedoph.spark_examples;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 /**
  * Criado por ph em 12/14/16.
  */
 @Data
+@Builder
+@ToString
 public class Person {
-
-    //id	name	gender	title	age	country	skill	university
-
+    private long   id;
     private String name;
+    private String gender;
     private int    age;
-
+    private String time;
+    private String uf;
 }
--- src/main/java/br/com/sitedoph/spark_examples/SparkBasicSessionReadingCSVApp.java
+++ src/main/java/br/com/sitedoph/spark_examples/SparkBasicSessionReadingCSVApp.java
@@ -1,11 +1,14 @@
 package br.com.sitedoph.spark_examples;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.PipelineModel;
 import org.apache.spark.ml.PipelineStage;
 import org.apache.spark.ml.evaluation.RegressionEvaluator;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.feature.VectorIndexer;
 import org.apache.spark.ml.feature.VectorIndexerModel;
 import org.apache.spark.ml.regression.GBTRegressionModel;
@@ -14,50 +17,60 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Criado por ph em 12/14/16.
  */
+@Slf4j
 public class SparkBasicSessionReadingCSVApp {
+
+    public static final  String[]     FEATURES_STRING_COLUMNS = new String[]{"gender", "time", "uf"};
+    public static final  Set<String>  featuresNumericColumns  = new HashSet<>(Collections.singletonList("age"));
+    private static final SparkSession spark                   = SparkSession
+            .builder()
+            .appName("Java Spark SQL basic example")
+            .config("spark.master", "local")
+            .getOrCreate();
 
     public static void main(String[] args) {
 
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("Java Spark SQL basic example")
-                .config("spark.master", "local")
-                .getOrCreate();
+        Dataset<Row> data = loadAndParseCSV("src/main/resources/people2.csv");
+        //data.printSchema();
+        //data.show();
 
+        /*
+            example:
+            +---+------+---+-----------------+-----------+---+
+            |age|gender| id|             name|       time| uf|
+            +---+------+---+-----------------+-----------+---+
+            | 68|  Male|  1|    Carl Matthews|   cruzeiro| MG|
+            | 28|  Male|  2|     Adam Johnson|   cruzeiro| MG|
+            | 43|Female|  3|Kimberly Mcdonald|  palmeiras| MG|
+            | 65|Female|  4|    Teresa Butler|chapecoense| SC|
+            | 61|Female|  5|  Michelle Peters|  palmeiras| DF|
+         */
 
-        // Load and parse the data file
-        JavaRDD<Person> peopleRDD = spark.read()
-                                         .option("header", "true")
-                                         .csv("src/main/resources/people2.csv")
-                                         .javaRDD()
-                                         .map(new Function<Row, Person>() {
-                                             @Override
-                                             public Person call(Row row) throws Exception {
+        writeToDisk(data);
 
-                                                 System.out.println(row);
+        data = convertStringColumnsToNumeric(data);
+        data = createFeaturesVector(data);
 
-                                                 Person person = new Person();
-                                                 person.setName(row.getString(0));
-                                                 person.setAge(Integer.parseInt(row.getString(1)));
-                                                 return person;
-                                             }
-                                         });
+        /*
+            +---+------+----+-------------------+-----------+---+--------------+------------+----------+-------------------+
+            |age|gender|  id|               name|       time| uf|indexed-gender|indexed-time|indexed-uf|           features|
+            +---+------+----+-------------------+-----------+---+--------------+------------+----------+-------------------+
+            | 68|  Male|   1|      Carl Matthews|   cruzeiro| MG|           1.0|         3.0|       2.0| [68.0,1.0,3.0,2.0]|
+            | 28|  Male|   2|       Adam Johnson|   cruzeiro| MG|           1.0|         3.0|       2.0| [28.0,1.0,3.0,2.0]|
+            | 43|Female|   3|  Kimberly Mcdonald|  palmeiras| MG|           0.0|         0.0|       2.0| [43.0,0.0,0.0,2.0]|
+            | 65|Female|   4|      Teresa Butler|chapecoense| SC|           0.0|         2.0|       1.0| [65.0,0.0,2.0,1.0]|
+            | 61|Female|   5|    Michelle Peters|  palmeiras| DF|           0.0|         0.0|       3.0| [61.0,0.0,0.0,3.0]|
+            | 10|Female|   6|     Phyllis Burton|   cruzeiro| GO|           0.0|         3.0|       0.0| [10.0,0.0,3.0,0.0]|
+         */
 
-        // Apply a schema to an RDD of JavaBeans to get a DataFrame
-        Dataset<Row> data = spark.createDataFrame(peopleRDD, Person.class);
-
-        data.show();
-
-        // Automatically identify categorical features, and index them.
-        // Set maxCategories so features with > 4 distinct values are treated as continuous.
-        VectorIndexerModel featureIndexer = new VectorIndexer()
-                .setInputCol("features")
-                .setOutputCol("indexedFeatures")
-                .setMaxCategories(4)
-                .fit(data);
+        VectorIndexerModel featureIndexer = indexFeatures(data);
 
         // Split the data into training and test sets (30% held out for testing).
         Dataset<Row>[] splits = data.randomSplit(new double[]{0.7, 0.3});
@@ -66,7 +79,7 @@ public class SparkBasicSessionReadingCSVApp {
 
         // Train a GBT model.
         GBTRegressor gbt = new GBTRegressor()
-                .setLabelCol("label")
+                .setLabelCol("indexed-time")
                 .setFeaturesCol("indexedFeatures")
                 .setMaxIter(10);
 
@@ -76,22 +89,102 @@ public class SparkBasicSessionReadingCSVApp {
         // Train model. This also runs the indexer.
         PipelineModel model = pipeline.fit(trainingData);
 
-        // Make predictions.
-        Dataset<Row> predictions = model.transform(testData);
+        // Make controlledTestPredictions.
+        Dataset<Row> controlledTestPredictions = model.transform(testData);
 
         // Select example rows to display.
-        predictions.select("prediction", "label", "features").show(5);
+        controlledTestPredictions.select("prediction", "indexed-time", "time", "features").show(5);
 
         // Select (prediction, true label) and compute test error.
         RegressionEvaluator evaluator = new RegressionEvaluator()
-                .setLabelCol("label")
+                .setLabelCol("indexed-time")
                 .setPredictionCol("prediction")
                 .setMetricName("rmse");
-        double rmse = evaluator.evaluate(predictions);
+        double rmse = evaluator.evaluate(controlledTestPredictions);
         System.out.println("Root Mean Squared Error (RMSE) on test data = " + rmse);
 
         GBTRegressionModel gbtModel = (GBTRegressionModel) (model.stages()[1]);
         System.out.println("Learned regression GBT model:\n" + gbtModel.toDebugString());
+
+        //Making predictions with unknown data:
+
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        log.info("%%%%%%%%%%%    Starting Predictions With Unknown Data    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        Dataset<Row> semTimeDS = loadAndParseCSV("src/main/resources/people2-sem-time.csv");
+//        semTimeDS = convertStringColumnsToNumeric(semTimeDS);
+//        semTimeDS = createFeaturesVector(semTimeDS);
+
+        semTimeDS.show(100);
+
+        final Dataset<Row> semTimePredictedDS = model.transform(semTimeDS);
+
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        semTimePredictedDS.show(100);
+
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        log.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+    }
+
+    private static Dataset<Row> loadAndParseCSV(String path) {
+        // Load and parse the data file
+        final JavaRDD<Person> personJavaRDD = spark.read()
+                                                   .option("header", "true")
+                                                   .csv(path)
+                                                   .javaRDD()
+                                                   .map((Function<Row, Person>) row -> Person.builder()
+                                                                                             .id(Long.parseLong(row.getString(0)))
+                                                                                             .name(row.getString(1))
+                                                                                             .gender(row.getString(2))
+                                                                                             .age(Integer.parseInt(row.getString(3)))
+                                                                                             .time(row.getString(4))
+                                                                                             .uf(row.getString(5))
+                                                                                             .build());
+
+        return spark.createDataFrame(personJavaRDD, Person.class);
+    }
+
+    private static void writeToDisk(Dataset<Row> data) {
+//        data.write().json("src/main/resources/data-people.json");
+//        data.write().format("libsvm").save("src/main/resources/data-people-libsvm.txt");
+    }
+
+    private static Dataset<Row> convertStringColumnsToNumeric(Dataset<Row> data) {
+        // Indexing is done to improve the execution times as comparing indexes
+        // is much cheaper than comparing strings/floats
+
+        for (String column : FEATURES_STRING_COLUMNS) {
+            final String outputCol = "indexed-" + column;
+            featuresNumericColumns.add(outputCol);
+            StringIndexer indexer = new StringIndexer()
+                    .setInputCol(column)
+                    .setOutputCol(outputCol);
+            data = indexer.fit(data).transform(data);
+        }
+        return data;
+    }
+
+    private static Dataset<Row> createFeaturesVector(Dataset<Row> data) {
+        // Create a vector from columns. Name the resulting vector as "features"
+        VectorAssembler vectorAssembler = new VectorAssembler();
+        vectorAssembler.setInputCols(featuresNumericColumns.toArray(new String[0])).setOutputCol("features");
+        data = vectorAssembler.transform(data);
+        return data;
+    }
+
+    private static VectorIndexerModel indexFeatures(Dataset<Row> data) {
+        // Automatically identify categorical features, and index them.
+        // Set maxCategories so features with > 4 distinct values are treated as continuous.
+        return new VectorIndexer()
+                .setInputCol("features")
+                .setOutputCol("indexedFeatures")
+                .setMaxCategories(4)
+                .fit(data);
     }
 
 }
